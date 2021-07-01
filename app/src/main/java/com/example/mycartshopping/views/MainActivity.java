898086f75647c619898c928c2a0f4bb38f8a3112
NermeenAlaa <nermena5566@gmail.com>
2021-07-01 03:18:27 +0200

    onlineApp

--- app/src/main/java/com/example/mycartshopping/views/MainActivity.java
+++ app/src/main/java/com/example/mycartshopping/views/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.mycartshopping.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.mycartshopping.R;
 
 public class MainActivity extends AppCompatActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btn_open);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.setVisibility(View.GONE);
+                FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.MainContainer,new CartFragment()).commit();
+            }
+        });
     }
 }
